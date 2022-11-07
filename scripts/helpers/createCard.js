5e42327d061dc141b7518751d213436a9056f667
Bogdan Sutuzhko <julfy-bs@yandex.ru>
2022-11-07 20:35:14 +0300

    feat: добавляет функционал, иконку и стили для удаления карточки.

--- scripts/helpers/createCard.js
+++ scripts/helpers/createCard.js
@@ -9,6 +9,7 @@ export const createCard = (card) => {
           <img src="${card.image}"
                alt=""
                class="card__image"/>
+          <button class="button card__delete" type="button" aria-label="Удалить"></button>
           <div class="card__footer">
             <h2 class="card__title">${card.name}</h2>
             <button class="button card__like" type="button" aria-label="Добавить отметку &#34;Понравилось&#34;"></button>
--- scripts/index.js
+++ scripts/index.js
@@ -2,8 +2,10 @@ import { renderCards } from './cards/renderCards.js';
 import { popupEdit } from './popup/popupEdit.js';
 import { popupAdd } from './popup/popupAdd.js';
 import { likeCard } from './cards/likeCard.js';
+import { deleteCard } from './cards/deleteCard.js';
 
 renderCards();
 popupEdit();
 popupAdd();
 likeCard();
+deleteCard();
