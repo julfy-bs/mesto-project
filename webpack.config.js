057a80ee3c9b0b52a08e83de254c81d27298414f
Bogdan Sutuzhko <julfy-bs@yandex.ru>
2023-01-13 14:27:20 +0300

    feat: добавляет html файлы в production сборку.

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
-
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 module.exports = {
   entry: { main: './src/index.js' },
   output: {
@@ -27,5 +28,11 @@ module.exports = {
         exclude: '/node_modules/'
       }
     ]
-  }
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html' // путь к файлу index.html
+    }),
+    new CleanWebpackPlugin(),
+  ] // добавьте массив
 };
