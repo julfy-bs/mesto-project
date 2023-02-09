1f0aceb91886e14e7055a3141548b8cdaa4a1302
Bogdan Sutuzhko <julfy-bs@yandex.ru>
2023-02-09 22:03:14 +0300

    feat: добавляет класс Profile.

---
+++ src/components/Profile.js
@@ -0,0 +1,141 @@
+export default class Profile {
+  constructor({ nameSelector, occupationSelector, avatarSelector }) {
+    this._profileNameElement = document.querySelector(nameSelector);
+    this._profileAboutElement = document.querySelector(occupationSelector);
+    this._profileAvatarElement = document.querySelector(avatarSelector);
+  }
+
+  getUserInfo() {
+    return {
+      name: this._profileNameElement.textContent,
+      about: this._profileAboutElement.textContent
+    };
+  }
+
+  setUserInfo(userData) {
+    const { name, about, avatar, id } = userData;
+    console.log({ name, about, avatar, id });
+    this._profileNameElement.textContent = name;
+    this._profileAboutElement.textContent = about;
+    this._profileAvatarElement.src = avatar;
+    this._userId = id;
+  }
+
+  updateUserInfo({ name, about }) {
+    this._profileNameElement.textContent = name;
+    this._profileAboutElement.textContent = about;
+  }
+
+  updateUserAvatar({ avatar }) {
+    this._profileAvatarElement.src = avatar;
+  }
+
+  getUserId() {
+    return this._userId;
+  }
+}
+
+
+// import { openPopup, handleSubmit } from './popup.js';
+// import { createCard, prependCard } from './card.js';
+// import { PROFILE, POPUP, FORM, EVENT } from './enum.js';
+//
+// const avatarPopup = document.querySelector(POPUP.TYPE_AVATAR);
+// const avatarPopupOpenButton = document.querySelector(PROFILE.AVATAR);
+// const avatarPopupForm = document.forms[FORM.NAME_AVATAR];
+// const avatarPopupLinkInput = avatarPopupForm.querySelector(FORM.INPUT_LINK);
+// const profilePopup = document.querySelector(POPUP.TYPE_PROFILE);
+// const profileName = document.querySelector(PROFILE.CONTENT_NAME);
+// const profileAvatar = document.querySelector(PROFILE.CONTENT_AVATAR);
+// const profileOccupation = document.querySelector(PROFILE.CONTENT_OCCUPATION);
+// const profilePopupOpenButton = document.querySelector(PROFILE.BUTTON_EDIT_PROFILE);
+// const profilePopupForm = document.forms[FORM.NAME_PROFILE];
+// const profilePopupNameInput = profilePopupForm.querySelector(FORM.INPUT_NAME);
+// const profilePopupOccupationInput = profilePopupForm.querySelector(FORM.INPUT_OCCUPATION);
+// const cardPopup = document.querySelector(POPUP.TYPE_CARD);
+// const cardPopupOpenButton = document.querySelector(PROFILE.BUTTON_ADD_CARD);
+// const cardPopupForm = document.forms[FORM.NAME_CARD];
+// const cardPopupTitleInput = cardPopupForm.querySelector(FORM.INPUT_TITLE);
+// const cardPopupLinkInput = cardPopupForm.querySelector(FORM.INPUT_LINK);
+//
+// const fillProfileInputs = () => {
+//   profilePopupNameInput.value = profileName.textContent;
+//   profilePopupOccupationInput.value = profileOccupation.textContent;
+// };
+//
+// const setProfileAvatar = (src) => {
+//   profileAvatar.setAttribute('src', src);
+// };
+//
+// const setProfileName = (name) => {
+//   profileName.textContent = name;
+// };
+//
+// const setProfileOccupation = (occupation) => {
+//   profileOccupation.textContent = occupation;
+// };
+//
+// const handleAvatarFormSubmit = (e) => {
+//   const submitAvatarRequest = () => {
+//     const avatar = avatarPopupLinkInput.value;
+//     return updateUserAvatar(avatar)
+//       .then((userData) => {
+//         setProfileAvatar(userData.avatar);
+//       });
+//   };
+//   handleSubmit(submitAvatarRequest, e, 'изменения аватара пользователя');
+// };
+//
+// const handleOpenAvatar = () => {
+//   openPopup(avatarPopup);
+// };
+//
+// const handleProfileFormSubmit = (e) => {
+//   const user = { name: profilePopupNameInput.value, about: profilePopupOccupationInput.value };
+//   const submitProfileRequest = () => {
+//     return updateUser(user)
+//       .then((userData) => {
+//         setProfileName(userData.name);
+//         setProfileOccupation(userData.about);
+//       });
+//   };
+//   handleSubmit(submitProfileRequest, e, 'редактирования информации профиля');
+// };
+//
+// const handleOpenProfile = () => {
+//   openPopup(profilePopup);
+//   fillProfileInputs();
+// };
+//
+// const handleCardFormSubmit = (e) => {
+//   const submitCardFormRequest = () => {
+//     const card = { name: cardPopupTitleInput.value, link: cardPopupLinkInput.value };
+//     return addCard(card)
+//       .then((card) => {
+//         const cardClone = createCard(card, card.owner._id);
+//         prependCard(cardClone);
+//       });
+//   };
+//   handleSubmit(submitCardFormRequest, e, 'cоздания карточки');
+//
+// };
+//
+// const handleCardPopupOpenButton = () => {
+//   openPopup(cardPopup);
+// };
+//
+// const addProfileListeners = () => {
+//   cardPopupOpenButton.addEventListener(EVENT.CLICK, handleCardPopupOpenButton);
+//   cardPopupForm.addEventListener(EVENT.SUBMIT, handleCardFormSubmit);
+//   profilePopupOpenButton.addEventListener(EVENT.CLICK, handleOpenProfile);
+//   profilePopupForm.addEventListener(EVENT.SUBMIT, handleProfileFormSubmit);
+//   avatarPopupOpenButton.addEventListener(EVENT.CLICK, handleOpenAvatar);
+//   avatarPopupForm.addEventListener(EVENT.SUBMIT, handleAvatarFormSubmit);
+// };
+//
+// export {
+//   addProfileListeners,
+//   setProfileAvatar,
+//   setProfileName,
+//   setProfileOccupation
+// };
--- src/index.js
+++ src/index.js
@@ -1,17 +1,16 @@
 import './styles/pages/index.css';
-import { config, EVENT, FORM, VALIDATION } from './components/enum.js';
+import { config, EVENT, FORM, PROFILE, VALIDATION } from './components/enum.js';
 import { createCard, prependCard } from './components/card.js';
-import { addProfileListeners, setProfileAvatar, setProfileName, setProfileOccupation } from './components/profile.js';
 import { enableValidation } from './components/validation.js';
 import { startLoader, endLoader } from './components/loader.js';
 import deleteCardService from './components/deleteCardService.js';
 import { createError } from './components/error.js';
 import Api from './components/Api.js';
+import Profile from './components/Profile.js';
 
 const cardDeletePopupForm = document.forms[FORM.NAME_DELETE];
-let userId = localStorage.getItem('userId') || null;
 
-addProfileListeners();
+// addProfileListeners();
 cardDeletePopupForm.addEventListener(EVENT.SUBMIT, (e) => deleteCardService.delete(e));
 
 enableValidation({
@@ -31,35 +30,15 @@ document.addEventListener('DOMContentLoaded', () => {
 
   api.getData()
     .then(([userData, cards]) => {
-      console.log(userData, cards);
-      setProfileAvatar(userData.avatar);
-      setProfileName(userData.name);
-      setProfileOccupation(userData.about);
-      userId = userData._id;
-      localStorage.setItem('userId', userId);
+      const user = new Profile({ nameSelector: PROFILE.CONTENT_NAME, occupationSelector: PROFILE.CONTENT_OCCUPATION, avatarSelector: PROFILE.CONTENT_AVATAR});
+      user.setUserInfo({ name: userData.name, about: userData.about, avatar: userData.avatar, id: userData._id  })
       cards
         .reverse()
         .forEach(card => {
-          const cardClone = createCard(card, userId);
+          const cardClone = createCard(card, user.getUserId());
           prependCard(cardClone);
         });
       endLoader();
     })
     .catch((error) => createError(error.status, `Ошибка получения информации о пользователе.`));
-  // Promise.all([getUser(), getCards()])
-  //   .then(([userData, cards]) => {
-  //     setProfileAvatar(userData.avatar);
-  //     setProfileName(userData.name);
-  //     setProfileOccupation(userData.about);
-  //     userId = userData._id;
-  //     localStorage.setItem('userId', userId);
-  //     cards
-  //       .reverse()
-  //       .forEach(card => {
-  //         const cardClone = createCard(card, userId);
-  //         prependCard(cardClone);
-  //       });
-  //     endLoader();
-  //   })
-  //   .catch((error) => createError(error.status, `Ошибка получения информации о пользователе.`));
 });
