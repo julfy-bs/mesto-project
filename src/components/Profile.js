8ede010bc98fc680e578f7b9d47d6ec14400e542
Bogdan Sutuzhko <julfy-bs@yandex.ru>
2023-02-21 18:33:59 +0300

    feat: рефакторит код обработчика слушателей профиля, удаляет неиспользуемый код в классе Validation.

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,38 +1,12 @@
-import { EVENT } from './enum.js';
-
 export default class Profile {
   constructor({
                 nameSelector,
                 occupationSelector,
                 avatarSelector,
-                avatarButtonSelector,
-                editButtonSelector,
-                addCardButtonSelector
               }) {
     this._profileNameElement = document.querySelector(nameSelector);
     this._profileAboutElement = document.querySelector(occupationSelector);
     this._profileAvatarElement = document.querySelector(avatarSelector);
-    this._profileAvatarParentElement = document.querySelector(avatarButtonSelector);
-    this._profileEditButton = document.querySelector(editButtonSelector);
-    this._profileAddCardButton = document.querySelector(addCardButtonSelector);
-  }
-
-  addAvatarListener(handleAvatarElementClick) {
-    this._profileAvatarParentElement.addEventListener(EVENT.CLICK, () => {
-      handleAvatarElementClick();
-    });
-  }
-
-  addEditButtonListener(handleEditButtonClick) {
-    this._profileEditButton.addEventListener(EVENT.CLICK, () => {
-      handleEditButtonClick();
-    });
-  }
-
-  addNewCardButtonListener(handleAddCardButtonClick) {
-    this._profileAddCardButton.addEventListener(EVENT.CLICK, () => {
-      handleAddCardButtonClick();
-    });
   }
 
   getUserInfo() {
